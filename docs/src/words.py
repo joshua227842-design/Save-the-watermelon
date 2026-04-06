5b7c6b9c75a291be00ed3ce42d92b3d464ca4c34
joshua227842-design <joshua227842@gmail.com>
2026-04-05 23:18:49 -0700

    Delete docs/src directory

--- docs/src/words.py
+++
@@ -1,8 +0,0 @@
-import random
-
-# Academic word list for the project
-WORD_DATABASE = ["PYTHON", "ALGORITHM", "VARIABLE", "FUNCTION", "DATABASE"]
-
-def get_random_word():
-    """Returns a random word from the database."""
-    return random.choice(WORD_DATABASE)
