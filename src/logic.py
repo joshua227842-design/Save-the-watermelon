97cce5ad51558680b91cad64687cb47dccce9252
joshua227842-design <joshua227842@gmail.com>
2026-04-05 23:54:58 -0700

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -1,1 +1,6 @@
-
+def check_victory(secret_word, guessed_letters):
+    """Checks if all letters in the secret word have been guessed."""
+    for letter in secret_word:
+        if letter not in guessed_letters:
+            return False
+    return True
